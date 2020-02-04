7ad7a96a611001f47ff890bbda6cbc2ba13f8c14
solr <solr@gmx.de>
2020-02-04 18:07:01 +0100

    initial

--- blog-example/workspace/src/PostEditor.js
+++ blog-example/workspace/src/PostEditor.js
@@ -1,9 +1,39 @@
 import React from "react";
 
-export default function HelloWorld() {
-  return (
-    <div className="Container">
-      <h1>Hello World!</h1>
-    </div>
-  );
+export default function PostEditor(props) {
+	const [title, setTitle] = React.useState(props.title || "");
+	const [body, setBody] = React.useState(props.body || "");
+
+	const clearDisabled = !title && !body;
+	const saveDisabled = !title || !body;
+
+	function clear() {
+		setTitle("");
+		setBody("");
+	}
+
+	return (
+		<>
+			<button onClick={() => props.onBackToList()}>Zurück</button>
+			<div className="Container">
+
+				<h1>Create Post</h1>
+
+				<label>
+					Title
+				<input value={title} onChange={e => setTitle(e.currentTarget.value)} />
+				</label>
+
+				<label>
+					Body
+				<textarea value={body} onChange={e => setBody(e.currentTarget.value)} />
+				</label>
+
+				<button disabled={clearDisabled} onClick={clear}>Clear</button>
+
+				<button disabled={saveDisabled} onClick={() => props.onSavePost({ title: title, body: body })}>Save</button>
+
+			</div>
+		</>
+	);
 }
--- blog-example/workspace/src/index.js
+++ blog-example/workspace/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import "./index.css";
-import PostEditor from "./PostEditor";
+import App from "./App";
 
-ReactDOM.render(<PostEditor />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
+
